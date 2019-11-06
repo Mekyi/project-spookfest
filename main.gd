38853a7d8bf26008653b39dcafa1dc25fa909234
unknown <teemu.tammisalo@student.lamk.fi>
2019-11-06 16:32:59 +0200

    added camera that zooms in on room and changes room depending on where the player went

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+signal camera_change
 var map_maker = load ("res://map_maker.gd").new()
 var room_map = []
 var Room = preload("res://room.tscn")
@@ -9,33 +10,48 @@ var Player = preload("res://Player.tscn")
 var room_size
 
 func _ready():
-    randomize()
-    map_maker._ready()
-    room_map = map_maker.room_map
-    for n in range(map_maker.roomWidth):
-        print(room_map[n])
-    make_rooms()
-    var start_room = randi()%($Rooms.get_child_count())+0
-    var pos = $Rooms.get_child(start_room).position
-    start_pos = pos
-    var player1 = Player.instance()
-    player1.start(Vector2(start_pos[0]+room_size[0]*tile_size/2, start_pos[1] + room_size[1]*tile_size/2))
-    $Startposition.add_child(player1)
-    
+	randomize()
+	map_maker._ready()
+	room_map = map_maker.room_map
+	for n in range(map_maker.roomWidth):
+		print(room_map[n])
+	make_rooms()
+	var start_room = randi()%($Rooms.get_child_count())+0
+	var pos = $Rooms.get_child(start_room).position
+	start_pos = pos
+	var player1 = Player.instance()
+	$Camera2D.start(pos)
+	player1.start(Vector2(start_pos[0]+room_size[0]*tile_size/2, start_pos[1] + room_size[1]*tile_size/2))
+	$Startposition.add_child(player1)
+	
 func make_rooms():
-    var size
-    var s = Room.instance()
-    room_size = s.get_size()					# checks room size to use
-    
-    for x in range(map_maker.roomWidth):
-        for y in range(map_maker.roomHeight):
-            if room_map[x][y] == 2:
-                var left = false
-                var up = false
-                if y-1 >= 0 and room_map[x][y-1] == 2:
-                    left = true
-                if x-1 >= 0 and room_map[x-1][y] == 2:
-                    up = true
-                var r = Room.instance()
-                r.make_room(Vector2(y*tile_size*room_size[0], x*tile_size*room_size[1]), up, left, room_size[0], room_size[1])
-                $Rooms.add_child(r)
+	var size
+	var s = Room.instance()
+	room_size = s.get_size()					# checks room size to use
+	
+	for x in range(map_maker.roomWidth):
+		for y in range(map_maker.roomHeight):
+			if room_map[x][y] == 2:
+				var left = false
+				var up = false
+				if y-1 >= 0 and room_map[x][y-1] == 2:
+					left = true
+				if x-1 >= 0 and room_map[x-1][y] == 2:
+					up = true
+				var r = Room.instance()
+				r.make_room(Vector2(y*tile_size*room_size[0], x*tile_size*room_size[1]), up, left, room_size[0], room_size[1])
+				$Rooms.add_child(r)
+				
+func _process(delta):
+	if $Startposition.get_child(0).position[0] < start_pos[0]:		#going left
+		start_pos = Vector2(start_pos[0]-tile_size*room_size[0], start_pos[1])
+		$Camera2D.move_camera(start_pos)
+	if $Startposition.get_child(0).position[0] > start_pos[0]+tile_size*room_size[0]:		#going right
+		start_pos = Vector2(start_pos[0]+tile_size*room_size[0], start_pos[1])
+		$Camera2D.move_camera(start_pos)
+	if $Startposition.get_child(0).position[1] < start_pos[1]:		#going up
+		start_pos = Vector2(start_pos[0], start_pos[1]-tile_size*room_size[1])
+		$Camera2D.move_camera(start_pos)
+	if $Startposition.get_child(0).position[1] > start_pos[1]+tile_size*room_size[1]:		#going down
+		start_pos = Vector2(start_pos[0], start_pos[1]+tile_size*room_size[1])
+		$Camera2D.move_camera(start_pos)
--- room_layout.gd
+++ room_layout.gd
@@ -1,27 +1,27 @@
 extends TileMap
 
 func _make_door(x, y, up, left):				# no need to check all directions. We can make double doors
-    
-    if left == true:							# creates doors to left
-        set_cell(0, y/2-1, 4, false, false, true)
-        set_cell(0, y/2, 4, false, true, true)
-        set_cell(-1, y/2-1, 4, true, false, true)
-        set_cell(-1, y/2, 4, true, true, true)
-    if up == true:								# creates doors to up
-        set_cell(x/2-1, 0, 4)
-        set_cell(x/2, 0, 4, true)
-        set_cell(x/2-1, -1, 4, false, true)
-        set_cell(x/2, -1, 4, true, true)
+	
+	if left == true:							# creates doors to left
+		set_cell(0, y/2-1, 4, false, false, true)
+		set_cell(0, y/2, 4, false, true, true)
+		set_cell(-1, y/2-1, 4, true, false, true)
+		set_cell(-1, y/2, 4, true, true, true)
+	if up == true:								# creates doors to up
+		set_cell(x/2-1, 0, 4)
+		set_cell(x/2, 0, 4, true)
+		set_cell(x/2-1, -1, 4, false, true)
+		set_cell(x/2, -1, 4, true, true)
 
 func _get_size():
-    var x = 0
-    var y = 0
-    var size
-    
-    while get_cell(x, 0) != -1:
-        x += 1
-    while get_cell(0, y) != -1:
-        y += 1
-    
-    size = Vector2(x,y)
-    return size
+	var x = 0
+	var y = 0
+	var size
+	
+	while get_cell(x, 0) != -1:
+		x += 1
+	while get_cell(0, y) != -1:
+		y += 1
+	
+	size = Vector2(x,y)
+	return size
