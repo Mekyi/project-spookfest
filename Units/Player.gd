399a35f0cf23153778738a57e84677af486488af
Mekyi <noreply>
2019-11-11 08:57:32 +0200

    Fixed unmerged changes

--- Units/Player.gd
+++ Units/Player.gd
@@ -5,19 +5,21 @@ export (int) var speed = 300
 var playerId
 var velocity = Vector2()
 
-func start(playerNumber):
-    playerId = playerNumber
+func start(pos, id):
+    position = pos
+    playerId = id
+    show()
 
-func get_input(playerId):
+func get_input():
     velocity = Vector2()
     if playerId == 1:
-        if Input.is_action_pressed('right'):
+        if Input.is_action_pressed('player1_right'):
             velocity.x += 1
-        if Input.is_action_pressed('left'):
+        if Input.is_action_pressed('player1_left'):
             velocity.x -= 1
-        if Input.is_action_pressed('down'):
+        if Input.is_action_pressed('player1_down'):
             velocity.y += 1
-        if Input.is_action_pressed('up'):
+        if Input.is_action_pressed('player1_up'):
             velocity.y -= 1
         velocity = velocity.normalized() * speed
     
@@ -28,5 +30,5 @@ func _process(delta):
         $AnimatedSprite.flip_h = velocity.x < 0
 
 func _physics_process(delta):
-    get_input(playerNumber)
+    get_input()
     velocity = move_and_slide(velocity)
