d31cf51d4dbe303fce48c4d00d68cd6d74d79136
RabbitTank <Mikael.Petrow@student.lamk.fi>
2019-11-25 11:56:18 +0200

    moving and idle animations

--- Units/SaltEnemy.gd
+++ Units/SaltEnemy.gd
@@ -5,9 +5,9 @@ var playerPosition
 var direction
 
 func start(pos):
-    position = pos
-    show()
+	position = pos
+	show()
 
 func _process(delta):
-    direction  = (get_node("../Player").position - position).normalized()
-    move_and_slide(direction * speed * 10 * delta)
+	direction  = (get_node("../Player").position - position).normalized()
+	move_and_slide(direction * speed * 10 * delta)
