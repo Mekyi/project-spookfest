b149e6c7ad8af7ac71e148d3a41f2feab0c1519e
unknown <teemu.tammisalo@student.lamk.fi>
2019-12-02 16:57:15 +0200

    changed collision layers

--- Units/SaltEnemy.gd
+++ Units/SaltEnemy.gd
@@ -4,8 +4,8 @@ export (int) var speed = 200
 var direction
 
 func start(pos):
-	position = pos
-	show()
+    position = pos
+    show()
 
 func _process(delta):
     direction  = (get_node("../../../Startposition/Player").get_global_position() - get_global_position()).normalized()
--- room.gd
+++ room.gd
@@ -2,20 +2,20 @@ extends RigidBody2D
 var Doors = load("res://room_layout.gd").new()
 
 func make_room(_pos, up, left, down, right, x, y):			#creates room at the right place and then puts doors
-	position = _pos
-	$room_layout._make_door(x, y, up, left, down, right)
+    position = _pos
+    $room_layout._make_door(x, y, up, left, down, right)
 
 func get_size():
-	var size = $room_layout._get_size()
-	return size
-	
+    var size = $room_layout._get_size()
+    return size
+    
 func close_doors():
-	$room_layout.close_doors()
+    $room_layout.close_doors()
 func open_doors():
-	$room_layout.open_doors()
-	
+    $room_layout.open_doors()
+    
 func get_enemy_spawns():
-	var spawnList = [];
-	for spawnpoint in $SpawnPoints.get_children():
-		spawnList.append(spawnpoint.position)
-	return spawnList
+    var spawnList = [];
+    for spawnpoint in $SpawnPoints.get_children():
+        spawnList.append(spawnpoint.position)
+    return spawnList
