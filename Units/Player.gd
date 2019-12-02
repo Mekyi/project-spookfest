ebc63dfccccbcc1d6ff89faa602d29e535c8e090
Mekyi <noreply>
2019-12-02 08:56:32 +0200

    Solved merge conflicts =)

--- Units/Player.gd
+++ Units/Player.gd
@@ -4,74 +4,71 @@ signal hitByEnemy()
 export (int) var speed = 300
 var health = 6
 
+var canTakeDamage = true
 var playerId
 var velocity = Vector2()	
 const TIME_PERIOD = 5
 var time = 0
 
 func start(pos, id):
-	position = pos
-	playerId = id
-	$AnimationPlayer.play("Move")
-	
-	show()
+    position = pos
+    playerId = id
+    $AnimationPlayer.play("Move")
+    
+    show()
 
 func timer(delta):
-	time += delta
-	if (velocity != Vector2(0,0)):
-		time = 0
-	if time > TIME_PERIOD:
-		$AnimationPlayer.play("Idle")
-		time = 0
+    time += delta
+    if (velocity != Vector2(0,0)):
+        time = 0
+    if time > TIME_PERIOD:
+        $AnimationPlayer.play("Idle")
+        time = 0
 
 func get_input():
-	velocity = Vector2()
-	if playerId == 1:
-		if Input.is_action_pressed('player1_right'):
-			$AnimationPlayer.stop();
-			velocity.x += 1
-			$Sprite_Idle.frame = 10
-		if Input.is_action_pressed('player1_left'):
-			$AnimationPlayer.stop();
-			velocity.x -= 1
-			$Sprite_Idle.frame = 14
-		if Input.is_action_pressed('player1_down'):
-			$AnimationPlayer.stop();
-			velocity.y += 1
-			$Sprite_Idle.frame = 8
-		if Input.is_action_pressed('player1_up'):
-			$AnimationPlayer.stop();
-			velocity.y -= 1
-			$Sprite_Idle.frame = 12
-		if velocity == Vector2(1,-1): #Up Right
-			$AnimationPlayer.stop();
-			$Sprite_Idle.frame = 11
-		if velocity == Vector2(-1,-1): #Up Left
-			$AnimationPlayer.stop();
-			$Sprite_Idle.frame = 13
-		if velocity == Vector2(1,1): #Down Right
-			$AnimationPlayer.stop();
-			$Sprite_Idle.frame = 9
-		if velocity == Vector2(-1,1): #Down Left
-			$AnimationPlayer.stop();
-			$Sprite_Idle.frame = 15
-			
-	elif playerId == 2:
-		velocity = Vector2(Input.get_joy_axis(0, JOY_AXIS_0), Input.get_joy_axis(0, JOY_AXIS_1))
-		
-	velocity = velocity.normalized() * speed * 10
-	
+    velocity = Vector2()
+    if playerId == 1:
+        if Input.is_action_pressed('player1_right'):
+            $AnimationPlayer.stop();
+            velocity.x += 1
+            $Sprite_Idle.frame = 10
+        if Input.is_action_pressed('player1_left'):
+            $AnimationPlayer.stop();
+            velocity.x -= 1
+            $Sprite_Idle.frame = 14
+        if Input.is_action_pressed('player1_down'):
+            $AnimationPlayer.stop();
+            velocity.y += 1
+            $Sprite_Idle.frame = 8
+        if Input.is_action_pressed('player1_up'):
+            $AnimationPlayer.stop();
+            velocity.y -= 1
+            $Sprite_Idle.frame = 12
+        if velocity == Vector2(1,-1): #Up Right
+            $AnimationPlayer.stop();
+            $Sprite_Idle.frame = 11
+        if velocity == Vector2(-1,-1): #Up Left
+            $AnimationPlayer.stop();
+            $Sprite_Idle.frame = 13
+        if velocity == Vector2(1,1): #Down Right
+            $AnimationPlayer.stop();
+            $Sprite_Idle.frame = 9
+        if velocity == Vector2(-1,1): #Down Left
+            $AnimationPlayer.stop();
+            $Sprite_Idle.frame = 15
+            
+    elif playerId == 2:
+        velocity = Vector2(Input.get_joy_axis(0, JOY_AXIS_0), Input.get_joy_axis(0, JOY_AXIS_1))
+        
+    velocity = velocity.normalized() * speed * 10
+    
 func _process(delta):
-	timer(delta)
-	if velocity.x != 0:
-		$AnimatedSprite.flip_v = false
-		$AnimatedSprite.flip_h = velocity.x < 0
+    timer(delta)
+    if velocity.x != 0:
+        $AnimatedSprite.flip_v = false
+        $AnimatedSprite.flip_h = velocity.x < 0
 
 func _physics_process(delta):
-<<<<<<< HEAD
-	get_input()
-	velocity = move_and_slide(velocity * delta)
-=======
     get_input()
     velocity = move_and_slide(velocity * delta)
 
@@ -83,4 +80,3 @@ func _on_Player_hitByEnemy():
         
 func get_location():
     return position
->>>>>>> 90694f88b1b8fc619ee2ca8d670398bb567858d3
--- main.gd
+++ main.gd
@@ -9,7 +9,6 @@ var Room = preload("res://room.tscn")
 var tile_size = 16
 var start_pos = Vector2(0,0)
 var Player = preload("res://Units/Player.tscn")
-var Enemy = preload("res://Units/SaltEnemy.tscn")
 
 var room_size
 var t = 0
@@ -32,67 +31,46 @@ func _ready():
     old_pos = start_pos
     
 func make_rooms():
-	var size
-	var s = Room.instance()
-	room_size = s.get_size()					# checks room size to use
-	
-	for x in range(map_maker.roomWidth):
-		for y in range(map_maker.roomHeight):
-			if room_map[x][y] == 2:
-				var left = false
-				var up = false
-				var down = false
-				var right = false
-				if y-1 >= 0 and room_map[x][y-1] == 2:
-					left = true
-				if y+1 <= map_maker.roomHeight-1 and room_map[x][y+1] == 2:
-					right = true
-				if x-1 >= 0 and room_map[x-1][y] == 2:
-					up = true
-				if x+1 <= map_maker.roomWidth-1 and room_map[x+1][y] == 2:
-					down = true 
-				var r = Room.instance()
-				r.make_room(Vector2(y*tile_size*room_size[0], x*tile_size*room_size[1]), up, left, down, right, room_size[0], room_size[1])
-				$Rooms.add_child(r)
-				
+    var size
+    var s = Room.instance()
+    room_size = s.get_size()					# checks room size to use
+    
+    for x in range(map_maker.roomWidth):
+        for y in range(map_maker.roomHeight):
+            if room_map[x][y] == 2:
+                var left = false
+                var up = false
+                var down = false
+                var right = false
+                if y-1 >= 0 and room_map[x][y-1] == 2:
+                    left = true
+                if y+1 <= map_maker.roomHeight-1 and room_map[x][y+1] == 2:
+                    right = true
+                if x-1 >= 0 and room_map[x-1][y] == 2:
+                    up = true
+                if x+1 <= map_maker.roomWidth-1 and room_map[x+1][y] == 2:
+                    down = true 
+                var r = Room.instance()
+                r.make_room(Vector2(y*tile_size*room_size[0], x*tile_size*room_size[1]), up, left, down, right, room_size[0], room_size[1])
+                $Rooms.add_child(r)
+                
 func _process(delta):
-	var new_pos
-	var direction
-	if $Startposition.get_child(0).position[0] < old_pos[0]:			# going left
-		new_pos = Vector2(old_pos[0]-tile_size*room_size[0], old_pos[1])
-		move_camera(new_pos, Vector2(-32, 0))
-	if $Startposition.get_child(0).position[0] > old_pos[0]+tile_size*room_size[0]:		#going right
-		new_pos = Vector2(old_pos[0]+tile_size*room_size[0], old_pos[1])
-		move_camera(new_pos, Vector2(32, 0))
-	if $Startposition.get_child(0).position[1] < old_pos[1]:		#going up
-		new_pos = Vector2(old_pos[0], old_pos[1]-tile_size*room_size[1])
-		move_camera(new_pos, Vector2(0, -32))
-	if $Startposition.get_child(0).position[1] > old_pos[1]+tile_size*room_size[1]:		#going down
-		new_pos = Vector2(old_pos[0], old_pos[1]+tile_size*room_size[1])
-		move_camera(new_pos, Vector2(0, 32))
-		
+    var new_pos
+    var direction
+    if $Startposition.get_child(0).position[0] < old_pos[0]:			# going left
+        new_pos = Vector2(old_pos[0]-tile_size*room_size[0], old_pos[1])
+        move_camera(new_pos, Vector2(-32, 0))
+    if $Startposition.get_child(0).position[0] > old_pos[0]+tile_size*room_size[0]:		#going right
+        new_pos = Vector2(old_pos[0]+tile_size*room_size[0], old_pos[1])
+        move_camera(new_pos, Vector2(32, 0))
+    if $Startposition.get_child(0).position[1] < old_pos[1]:		#going up
+        new_pos = Vector2(old_pos[0], old_pos[1]-tile_size*room_size[1])
+        move_camera(new_pos, Vector2(0, -32))
+    if $Startposition.get_child(0).position[1] > old_pos[1]+tile_size*room_size[1]:		#going down
+        new_pos = Vector2(old_pos[0], old_pos[1]+tile_size*room_size[1])
+        move_camera(new_pos, Vector2(0, 32))
+        
 func move_camera(new_pos, direction):
-<<<<<<< HEAD
-		var player_pos
-		var room_pos
-		get_tree().paused = true
-		$Camera2D.set_enable_follow_smoothing(true)
-		$Camera2D.position = new_pos
-		$Startposition.get_child(0).position += direction
-		player_pos = $Startposition.get_child(0).position
-		for room in $Rooms.get_child_count():
-			room_pos = $Rooms.get_child(room).position
-			if room_pos[0] < player_pos[0] and player_pos[0] < room_pos[0]+(room_size[0]*tile_size) and room_pos[1] < player_pos[1] and player_pos[1] < room_pos[1]+(room_size[1]*tile_size):
-				current_room = room
-				break
-		yield(get_tree().create_timer(1.0), "timeout")
-		$Rooms.get_child(current_room).close_doors()
-		get_tree().paused = false
-		old_pos = new_pos
-		yield(get_tree().create_timer(5.0), "timeout")
-		$Rooms.get_child(current_room).close_doors()
-	
-=======
     var player_pos
     var room_pos
     get_tree().paused = true
@@ -101,10 +79,10 @@ func move_camera(new_pos, direction):
     $Startposition.get_child(0).position += direction
     player_pos = $Startposition.get_child(0).position
     for room in $Rooms.get_child_count():
-        room_pos = $Rooms.get_child(room).position
-        if room_pos[0] < player_pos[0] and player_pos[0] < room_pos[0]+(room_size[0]*tile_size) and room_pos[1] < player_pos[1] and player_pos[1] < room_pos[1]+(room_size[1]*tile_size):
-            current_room = room
-            break
+            room_pos = $Rooms.get_child(room).position
+            if room_pos[0] < player_pos[0] and player_pos[0] < room_pos[0]+(room_size[0]*tile_size) and room_pos[1] < player_pos[1] and player_pos[1] < room_pos[1]+(room_size[1]*tile_size):
+                    current_room = room
+                    break
     yield(get_tree().create_timer(1.0), "timeout")
     spawn_enemies($Rooms.get_child(current_room))
     $Rooms.get_child(current_room).close_doors()
@@ -121,4 +99,3 @@ func spawn_enemies(room):
         enemy.position = spawn
         room.add_child(enemy)
     print(spawnLocations)
->>>>>>> 90694f88b1b8fc619ee2ca8d670398bb567858d3
