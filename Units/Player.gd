a588b6474db928533623c0d3c0d24fcc19e00aa1
RabbitTank <Mikael.Petrow@student.lamk.fi>
2019-12-06 01:33:34 +0200

    Rotation

--- Units/Player.gd
+++ Units/Player.gd
@@ -11,6 +11,7 @@ var velocity = Vector2()
 const TIME_PERIOD = 5
 var time = 0
 
+
 func start(pos, id):
 	position = pos
 	playerId = id
@@ -30,28 +31,43 @@ func timer(delta):
 		$AnimationPlayer.play("Idle")
 		time = 0
 
+func turning():
+	var rot = get_global_mouse_position().angle_to_point(position)
+	print("rotation = ", rot)
+	if rot < 1.9625 && rot > 1.1775:
+			$Sprite_Idle.frame = 8
+	if rot < 1.1775 && rot > 0.392:
+			$Sprite_Idle.frame = 9
+	if rot < 0.392 && rot > -0.3925:
+			$Sprite_Idle.frame = 10
+	if rot < -0.3925 && rot > -1.1775:
+		$AnimationPlayer.stop()
+		$Sprite_Idle.frame = 11
+	if rot < -1.1775 && rot > -1.9625:
+		$AnimationPlayer.stop()
+		$Sprite_Idle.frame = 12
+	if rot < -1.9625 && rot > -2.7475:
+		$AnimationPlayer.stop()
+		$Sprite_Idle.frame = 13
+	if rot < 2.7475 && rot > 1.9625:
+		$AnimationPlayer.stop()
+		$Sprite_Idle.frame = 15
+	if rot < -2.7475 || rot > 2.7475:
+		$AnimationPlayer.stop()
+		$Sprite_Idle.frame = 14
+
 func get_input():
 	velocity = Vector2()
 	if playerId == 1:
 		if Input.is_action_pressed('player1_right'):
-			$AnimationPlayer.stop();
 			velocity.x += 1
-			$Sprite_Idle.frame = 10
 		if Input.is_action_pressed('player1_left'):
-			$AnimationPlayer.stop();
 			velocity.x -= 1
-			$Sprite_Idle.frame = 14
 		if Input.is_action_pressed('player1_down'):
-			$AnimationPlayer.stop();
 			velocity.y += 1
-			$Sprite_Idle.frame = 8
 		if Input.is_action_pressed('player1_up'):
-			$AnimationPlayer.stop();
 			velocity.y -= 1
 		if Input.is_action_just_pressed('player1_shoot'):
-			var posX = get_global_mouse_position()[0] - get_location()[0]
-			print(posX)
-			print(get_global_mouse_position())
 			$AnimatedSprite.get_child(0).attack(get_location(), get_global_mouse_position())
 			if PlayerVariables.CurrentAttack == "basic_attack":
 				get_node("WaveSoundPlayer").play()
@@ -63,19 +79,6 @@ func get_input():
 			else:
 				PlayerVariables.CurrentAttack = "basic_attack"
 			print("SWITCH!")
-			$Sprite_Idle.frame = 12
-		if velocity == Vector2(1,-1): #Up Right
-			$AnimationPlayer.stop();
-			$Sprite_Idle.frame = 11
-		if velocity == Vector2(-1,-1): #Up Left
-			$AnimationPlayer.stop();
-			$Sprite_Idle.frame = 13
-		if velocity == Vector2(1,1): #Down Right
-			$AnimationPlayer.stop();
-			$Sprite_Idle.frame = 9
-		if velocity == Vector2(-1,1): #Down Left
-			$AnimationPlayer.stop();
-			$Sprite_Idle.frame = 15
 			
 	elif playerId == 2:
 		velocity = Vector2(Input.get_joy_axis(0, JOY_AXIS_0), Input.get_joy_axis(0, JOY_AXIS_1))
@@ -84,6 +87,7 @@ func get_input():
 	
 func _process(delta):
 	timer(delta)
+	turning()
 	if velocity.x != 0:
 		$AnimatedSprite.flip_v = false
 		$AnimatedSprite.flip_h = velocity.x < 0
@@ -91,6 +95,7 @@ func _process(delta):
 func _physics_process(delta):
 	get_input()
 	velocity = move_and_slide(velocity * delta)
+	
 
 
 func _on_Player_hitByEnemy():
